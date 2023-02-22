e8d31a764d8bf4eee5b26cf9f8d7c8a881f1dd9b
bittmoon <ahmed.barbarycloud@icloud.com>
2023-02-22 04:00:39 -0800

    prints 9 times table starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,7 +16,7 @@ void times_table(void)
 		_putchar(' ');
 		for (column = 1; column <= 9; column++)
 		{
-			d = (row * column);
+			result = (row * column);
 			if ((result / 10) > 0)
 			{
 				_putchar((result / 10) + '0');
