4cb00f6cc35d0a2717bef8a95081d38ac13ea980
bittmoon <ahmed.barbarycloud@icloud.com>
2023-02-22 03:24:57 -0800

    check the sign of n

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include"main.h"
+/**
+ * print_sign - printin the sign
+ *
+ * @n: number
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	else
+	{
+		if (n < 0)
+		{
+			return (-1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+
+}
