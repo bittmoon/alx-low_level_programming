16d1edaaa70199b194e9b4b4973f99248123966d
bittmoon <ahmed.barbarycloud@icloud.com>
2023-02-22 02:45:13 -0800

    check char

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * _islower - checks for lowercase
+ * @c: char input
+ * Return: 1 if lower ,0 if upper
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
